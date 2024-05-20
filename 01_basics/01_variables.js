43502137135c50eaf8315e1e026c1ca1348f57e4
Gitnutan1195 <nutans1195@gmail.com>
2024-05-20 05:47:09 +0000

    commit variable declear

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1221
+let accountEmail ="nutans@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+
+// accountId = 12
+accountEmail = "sing@gmail.com"
+accountPassword = "21423o"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
