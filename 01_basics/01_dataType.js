b36225c5dae5aed91ae49717478de153a0d4b098
Gitnutan1195 <nutans1195@gmail.com>
2024-05-20 06:12:54 +0000

    datatypes

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,22 @@
+"use strict" //use all js code ad newer version
+//alert(3+3)  //we are using node js not browser
+console.log(3+3)
+console.log("nutan")
+
+let name= "nutan"
+let age = 18
+let inLoggedIn = true
+let state = null
+
+console.log(state)
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean =>true/false
+//null =>standalone value
+//undefined => not assigne value\
+//symbol => unique
+//object
+// null typeof is object
+console.log(typeof null)
+console.log(typeof undefined)
