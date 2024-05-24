2edeb4b9cc5693322a206d1d04bcf07de49372c1
Gitnutan1195 <nutans1195@gmail.com>
2024-05-24 05:05:40 +0000

    operations

--- 01_basics/01_dataType.js
+++ 01_basics/01_dataType.js
@@ -1,14 +1,17 @@
 "use strict" //use all js code ad newer version
 //alert(3+3)  //we are using node js not browser
-console.log(3+3)
-console.log("nutan")
+
 
 let name= "nutan"
 let age = 18
 let inLoggedIn = true
 let state = null
+let score = 33;
+let valueNumber = String(score);
+let loggedIn = "nutan";
+let booleanNumber = Boolean(loggedIn);
+console.log(booleanNumber);
 
-console.log(state)
 //number => 2 to power 53
 //bigint
 //string = ""
@@ -20,3 +23,4 @@ console.log(state)
 // null typeof is object
 console.log(typeof null)
 console.log(typeof undefined)
+console.log( typeof valueNumber);
---
+++ 01_basics/01_operationDataType.js
@@ -0,0 +1,14 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log("2" +2);
+console.log("1"+ 2+ 2);
+console.log(1+ 2+ "2");
+console.log(1+ "2"+ 2);
+console.log(+"");
+// let num1, num2,num3;
+// num1 = num2= num3=2+2;
+
+let gameCounter =100;
+++gameCounter;
+console.log(gameCounter);
