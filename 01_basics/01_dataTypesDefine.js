ab7a7d737682368af0d46a05a189318a638bb5c0
Gitnutan1195 <nutans1195@gmail.com>
2024-06-03 11:30:20 +0000

    datatype definition

---
+++ 01_basics/01_dataTypesDefine.js
@@ -0,0 +1,14 @@
+//premitive 
+//7 types: String, number, boolean, null, undefine ,symbol,bigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+const myFunction = function(){
+    console.log("nutan");
+}
+
+
+//Reference type(non-premetive)
+//Array, objects ,function 
